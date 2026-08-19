e428d6974ad6027dfd6f00ad1336c8b2044509ca
tillorly <tillorly@users.noreply.gihub.com>
2026-08-19 22:20:52 +0200

    fix

--- hosts/osgiliath/default.nix
+++ hosts/osgiliath/default.nix
@@ -28,7 +28,7 @@
   programs.thunar.plugins = with pkgs; [ # was pkgs.xfce
     thunar-archive-plugin
     thunar-volman
-  ];z
+  ];
 
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
